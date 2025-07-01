b62bd1082f4e2f1835d4ca91245b25840756fb06
ソヌ <sonusid1325@gmail.com>
2025-07-01 19:08:47 +0530

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             </div>
           </div>
 
-          <NetworkSwitcher />
+          {/* <NetworkSwitcher /> */}
           <ConnectionStatus />
           <SOLBalance />
           <TroubleshootingHelp />
